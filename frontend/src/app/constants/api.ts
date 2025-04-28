03fd90adef973847559b8fe058a39698542ce46a
Cristiano Fernandes <cristiano.fernandes.of@gmail.com>
2025-04-27 22:56:00 -0300

    Criado constante API

---
+++ frontend/src/app/constants/api.ts
@@ -0,0 +1,9 @@
+const baseApiUrl = 'http://localhost:3000';
+
+const developer = `${baseApiUrl}/developer`;
+const github = `${baseApiUrl}/github`;
+
+export const API = {
+  github,
+  developer,
+};
